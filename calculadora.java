43d21ec1d2cb506a3821ba7ee0ece3cb454a509c
renan13333 <124295549+renan13333@users.noreply.github.com>
2023-02-09 15:15:19 -0400

    calculadora

---
+++ calculadora.java
@@ -0,0 +1,51 @@
+
+import static javax.management.Query.in;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Aluno
+ */
+public class calculadora {
+    
+Scanner scan=new scanner (syntem.int);
+
+private static readable in;
+
+
+public system(in);
+
+
+
+private void error(){
+    throw new UnsupprtedOperationException("NOT SUPPORTED YET.")
+            
+            }
+
+private static class scanner    
+
+
+public system (in) {
+    this.<error>= <error>;
+    
+    }
+.    
+private static reable in ;
+public scanner ()
+        
+        private static in{
+    
+    
+   public in ()
+
+       
+        }
+    }
+
+  
+
